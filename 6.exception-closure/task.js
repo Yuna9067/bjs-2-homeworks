d1dc878b80286dca7c9bc96a8f9d82c0a1837cbe
yana <ru.jin2227@gmail.com>
2026-05-10 05:25:09 +0300

    task is completed

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,54 @@
-﻿
+﻿function parseCount (value) {
+    const parsed = Number.parseFloat(value);
+
+    if (Number.isNaN(parsed)) {
+        throw new Error('Невалидное значение');
+    }
+
+    return parsed;
+}
+
+function validateCount (value) {
+    try {
+        return parseCount(value);
+    } catch (error) {
+        return error;
+    }
+}
+
+class Triangle {
+    constructor(b, t, s) {
+        if(b + t <= s || b + s <= t || t + s <= b) {
+            throw new Error('Треугольник с такими сторонами не существует')
+        };
+        this.b = b;
+        this.t = t;
+        this.s = s;
+    }
+
+    get perimeter() {
+        return this.b + this.t + this.s;
+    }
+
+    get area() {
+        const p = this.perimeter / 2;
+        const areaStr = Math.sqrt(p * (p - this.b) * (p - this.t) * (p - this.s)).toFixed(3);
+        const area = +areaStr;
+        return area;
+    }
+}
+
+function getTriangle(b, t, s) {
+    try {
+        return new Triangle(b, t, s);
+    } catch (error) {
+        return {
+            get area () {
+                return 'Ошибка! Треугольник не существует';
+            },
+            get perimeter () {
+                return 'Ошибка! Треугольник не существует';
+            }
+        };
+    }
+}
